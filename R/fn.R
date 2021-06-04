4169b8f91099b0573ac10fef6200fbe41e809ccd
Lina Benson <lina.benson@ki.se>
2021-06-04 15:43:58 +0200

    fix for v 0.0.15

--- R/fn.R
+++ R/fn.R
@@ -21,7 +21,7 @@ fn <- function(x, dig = 0, p = FALSE, pequalsign = FALSE, ...) {
   if (p) {
     out <- replace(
       out,
-      out == paste0("0.", paste0(rep(0, dig), collapse = "")), paste0("<0.", paste0(rep(0, max(dig - 1), 0), collapse = ""), "1")
+      out == paste0("0.", paste0(rep(0, dig), collapse = "")), paste0("<0.", paste0(rep(0, max(dig - 1, 0)), collapse = ""), "1")
     )
     if (pequalsign) {
       out <- dplyr::if_else(stringr::str_detect(out, "<", negate = TRUE), paste0("=", out), out)
